d6907f1217342d5cc26661afa7864484a9014fb5
R0byH <cesar.herbas@gmail.com>
2025-10-09 17:54:02 -0400

    Funcional

--- NetIdentity/Data/ApplicationDbContext.cs
+++ NetIdentity/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using NetIdentity.Models;
 
 namespace NetIdentity.Data
 {
@@ -8,6 +9,22 @@ namespace NetIdentity.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Product>().Property(p => p.Price).HasPrecision(18, 2); ;
+
+            builder.Entity<Order>()
+                .HasMany(o => o.Items)
+                .WithOne(i => i.Order)
+                .HasForeignKey(i => i.OrderId);
         }
     }
 }
